34c3f79b4bbb783dd38a769d8878803e8a40b1d7
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-08-26 18:05:15 +0530

    Add files via upload

---
+++ Discord/discord.sh
@@ -0,0 +1,10 @@
+dir=$PWD
+mkdir ~/Discord
+cp -r -v $PWD/* ~/Discord
+
+cp -v ~/Discord/Discord.desktop ~/.local/share/applications/ 
+
+
+
+
+~/Discord/flashpeak-slimjet --app=https://discord.com/app
